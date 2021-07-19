6d26050d1664924677c40f6b69fa9c74453880f3
suey0128 <syui0128@gmail.com>
2021-07-19 10:42:28 -0700

    commit tree cf816e9cf93ba75eaa13c6f70ba8cdcc7a1d6678

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
 
   def index
+    # byebug
     students = Student.all
+
+    if params[:name]
+      students = Student.all.select { |student| student.first_name == params[:name].capitalize || student.last_name == params[:name].capitalize}
+      # students = "hey"
+    end    
+
     render json: students
   end
+
+
+  def show
+    student = Student.find_by(id: params[:id])
+    render json: student
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/students', to: 'students#index'
   get '/students/grades', to: 'students#grades'
   get '/students/highest-grade', to: 'students#highest_grade'
+  get '/students/:id', to: 'students#show'
+  # get '/students/*query', to: 'students#show_by_name'
+  # get '/students?name=:name', to: 'students#show_by_name'
 end
